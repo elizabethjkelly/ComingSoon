48105442d9b3ba89265f7ebf5943c98a10323bd0
Francois Joubert <francois.joubert@opsi.co.za>
2017-06-22 09:31:17 +0200

    Added a movies barrel.

--- AngularSample/wwwroot/app/components/index.ts
+++ AngularSample/wwwroot/app/components/index.ts
@@ -1,14 +1,7 @@
 ﻿import { LayoutComponent } from './layout/layout.component';
-import { MovieFilterComponent } from './movie-filter/movie-filter.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieTileComponent } from './movie-tile/movie-tile.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MOVIE_COMPONENTS } from './movies/index';
 
 export const APP_COMPONENTS = [
   LayoutComponent,
-
-  MovieFilterComponent,
-  MovieListComponent,
-  MovieTileComponent,
-  MovieDetailsComponent
+  MOVIE_COMPONENTS
 ];
---
+++ AngularSample/wwwroot/app/components/movies/index.ts
@@ -0,0 +1,11 @@
+﻿import { MovieFilterComponent } from './movie-filter/movie-filter.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieTileComponent } from './movie-tile/movie-tile.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+
+export const MOVIE_COMPONENTS = [
+  MovieFilterComponent,
+  MovieListComponent,
+  MovieTileComponent,
+  MovieDetailsComponent
+];
