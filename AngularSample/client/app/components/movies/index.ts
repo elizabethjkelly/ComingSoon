fb9e4874e0c0eff079462ac147947ece6148ce64
Francois Joubert <francois.joubert@opsi.co.za>
2017-12-12 16:31:32 +0200

    Added movie details.

--- AngularSample/client/app/components/movies/index.ts
+++ AngularSample/client/app/components/movies/index.ts
@@ -1,7 +1,9 @@
 ﻿import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieTileComponent } from './movie-tile/movie-tile.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 export const MOVIE_COMPONENTS = [
   MovieListComponent,
-  MovieTileComponent
+  MovieTileComponent,
+  MovieDetailsComponent
 ];
---
+++ AngularSample/client/app/components/movies/movie-details/movie-details.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: 'movie-details.html',
+  styleUrls: ['movie-details.css']
+})
+export class MovieDetailsComponent
+{
+  @Input()
+  public movie;
+}
