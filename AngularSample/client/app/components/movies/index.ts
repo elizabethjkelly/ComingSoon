122411a54520b698109521e9499ecab32fe33140
Francois Joubert <francois.joubert@opsi.co.za>
2017-12-13 09:51:50 +0200

    Added filtering.

--- AngularSample/client/app/components/movies/index.ts
+++ AngularSample/client/app/components/movies/index.ts
@@ -1,9 +1,11 @@
 ﻿import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieTileComponent } from './movie-tile/movie-tile.component';
+import { MovieFilterComponent } from './movie-filter/movie-filter.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 export const MOVIE_COMPONENTS = [
   MovieListComponent,
   MovieTileComponent,
+  MovieFilterComponent,
   MovieDetailsComponent
 ];
--- AngularSample/client/app/components/movies/movie-list/movie-list.component.ts
+++ AngularSample/client/app/components/movies/movie-list/movie-list.component.ts
@@ -8,16 +8,40 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MovieListComponent
 {
+  public get filter() { return this._filter; }
+  public set filter(value)
+  {
+    this._filter = value;
+    this.updateList();
+  }
+  private _filter = { keyword: null, minRating: 0 };
+
   public movies: any[];
 
   public selectedMovie: any;
+
+  private _allMovies: any[];
 
   constructor(private _http: HttpClient)
   {
     _http.get<any[]>('movies/movies.json')
       .subscribe(
-      movies => this.movies = movies,
+      movies => this.updateList(movies),
       err => console.error(err));
+  }
+
+  public updateList(movies?)
+  {
+    if (movies)
+    {
+      this._allMovies = movies;
+    }
+
+    this.movies = this._allMovies
+      .filter(movie =>
+          (!this.filter.keyword || movie.name.toLowerCase().includes(this.filter.keyword)) &&
+          movie.rating >= this.filter.minRating)
+      .sort((a, b) => a.name < b.name ? -1 : 1);
   }
 
   public onSelect(event)
